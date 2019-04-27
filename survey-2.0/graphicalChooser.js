1c6071bcc606c39bc0230c129ae72eaf6cddbb0a
Olivia Yang <yang4862@umn.edu>
2019-04-26 22:37:16 -0500

    q

--- survey-2.0/graphicalChooser.js
+++ survey-2.0/graphicalChooser.js
@@ -15,9 +15,11 @@ function init() {
   });
 
   $('#house').on('drop', (evt, ui) => {
+
     let droppedName = $(ui.draggable).find('.appliance-title').html();
     let droppedId = $(ui.draggable).attr('id');
     let value = $(ui.draggable).find('input').val();
+    console.log(value);
     let toCreate = $('<p/>', {id: `house-${droppedId}`, text: droppedName + ' ' + value});
     if ($(evt.target).find(`p#house-${droppedId}`).length == 0) {
       $(evt.target).append(toCreate);
